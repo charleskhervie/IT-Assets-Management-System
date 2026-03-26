9acef999e83648a365cc2e213f27ebfa5d550b56
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2026-03-26 18:33:31 +0800

    add db util for connection to db

---
+++ src/main/java/dao/dao_util/DBUtil.java
@@ -0,0 +1,29 @@
+package dao.dao_util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/itams"
+    + "?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final CredentialManager envFile = new CredentialManager();
+    private DBUtil() {}
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Properties props = envFile.load();
+            return DriverManager.getConnection(URL, props.getProperty("username"), props.getProperty("password"));
+        } catch (Exception e) {
+            throw new SQLException("Failed to connect: " + e.getMessage());
+        }
+    }
+
+    public static boolean isValid(String user, String pass) {
+        try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+}
