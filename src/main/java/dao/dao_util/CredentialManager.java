8cb8ca096bf5e3decdc2247c81457599c0c19654
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2026-03-26 18:58:09 +0800

    indented and package error

--- src/main/java/dao/dao_util/CredentialManager.java
+++ src/main/java/dao/dao_util/CredentialManager.java
@@ -1,36 +1,36 @@
-    package util;
+package dao.dao_util;
 
-    import java.io.*;
-    import java.util.Properties;
+import java.io.*;
+import java.util.Properties;
 
-    public class CredentialManager {
-        private static final String ENV_FILE = "app.env";
+public class CredentialManager {
+    private static final String ENV_FILE = "app.env";
 
-        public CredentialManager() {} 
+    public CredentialManager() {} 
 
-        public void write(String user, String pass, String mode) throws IOException {
-            Properties props = new Properties();
-            props.setProperty("username", user);
-            props.setProperty("password", pass);
-            props.setProperty("app_mode", mode);
-            try (OutputStream output = new FileOutputStream(ENV_FILE)) {
-                props.store(output, null);
-            }
-        }
-
-        public Properties load() throws IOException {
-            Properties props = new Properties();
-            try (InputStream input = new FileInputStream(ENV_FILE)) {
-                props.load(input);
-                return props;
-            }
-        }
-
-        public boolean exists() {
-            return new File(ENV_FILE).exists();
-        }
-
-        public void clear() {
-            new File(ENV_FILE).delete();
+    public void write(String user, String pass, String mode) throws IOException {
+        Properties props = new Properties();
+        props.setProperty("username", user);
+        props.setProperty("password", pass);
+        props.setProperty("app_mode", mode);
+        try (OutputStream output = new FileOutputStream(ENV_FILE)) {
+            props.store(output, null);
         }
     }
+
+    public Properties load() throws IOException {
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream(ENV_FILE)) {
+            props.load(input);
+            return props;
+        }
+    }
+
+    public boolean exists() {
+        return new File(ENV_FILE).exists();
+    }
+
+    public void clear() {
+        new File(ENV_FILE).delete();
+    }
+}
